0b39e36e1181430b2c540a7c61cc9794d8e99cbf
meruleo165 <merula651@gmail.com>
2026-01-12 12:38:10 +0100

    Add hazard data for cover types in environment

--- js/data_environment.js
+++ js/data_environment.js
@@ -141,3 +141,43 @@ data_environment_cover = [
         ]
     }
 ]
+data_environment_hazard = [
+    {
+        title: "Cadere",
+        optional: "Standard rule",
+        icon: "broken-shield",
+        subtitle: "1d6 per 3m (max 20d6)",
+        description: "A target has half cover if an obstacle blocks at least half of its body",
+        reference: "PHB, pg. 196.",
+        bullets: [
+            "The obstacle might be a low wall, a large piece of furniture, a narrow tree trunk, or a creature, whether that creature is an enemy or a friend.",
+            "A target with half cover has a <b>+2 bonus to AC and Dexterity saving throws</b>.",
+            "If a target is behind multiple sources of cover, only the most protective degree of cover applies"
+        ]
+    },
+    {
+        title: "Three-quarters cover",
+        optional: "Standard rule",
+        icon: "cracked-shield",
+        subtitle: "Portcullis, arrow slit",
+        description: "A target has three-quarters cover if about three-quarters of it is covered by an obstacle",
+        reference: "PHB, pg. 196.",
+        bullets: [
+            "The obstacle might be a portcullis, an arrow slit, or a thick tree trunk.",
+            "A target with three-quarters cover has a <b>+5 bonus to AC and Dexterity saving throws</b>.",
+            "If a target is behind multiple sources of cover, only the most protective degree of cover applies"
+        ]
+    },
+    {
+        title: "Full cover",
+        optional: "Standard rule",
+        icon: "shield",
+        subtitle: "Completely concealed",
+        description: "A target has total cover if it is completely concealed by an obstacle",
+        reference: "PHB, pg. 196.",
+        bullets: [
+            "A target with total cover <b>can’t be targeted directly</b> by an attack or a spell, although some spells can reach such a target by including it in an area of effect.",
+            "If a target is behind multiple sources of cover, only the most protective degree of cover applies"
+        ]
+    }
+]
