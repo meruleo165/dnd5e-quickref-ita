694f37a36e4e2cf4d9976551f3034f5664c55ddc
meruleo165 <merula651@gmail.com>
2026-01-12 11:41:08 +0100

    Update titles and descriptions for reaction rules

--- js/data_reaction.js
+++ js/data_reaction.js
@@ -37,7 +37,22 @@ data_reaction = [
         ]
     },
     {
-        title: "Presa **",
+        title: "Identifica un incantesimo*",
+        optional: "Optional rule",
+        icon: "magic-swirl",
+        subtitle: "Identifica un incantesimo che viene lanciato",
+        description: "Usa la tua reazione e una prova di Intelligenza (Arcano) per identificare un incantesimo mentre viene lanciato.",
+        reference: "XGE, pg. 85.",
+        bullets: [
+            "(Regola Opzionale)"
+            "Innesco: Una creatura che puoi vedere lancia un incantesimo.",
+            "Reazione: Effettua una prova di Intelligenza (Arcano). La CD è pari a 15 + il livello dell'incantesimo.",
+            "In caso di successo, identifichi l'incantesimo. Questa prova non rivela il livello dello slot incantesimo utilizzato per lanciarlo.",
+            "Usare la tua reazione per identificare l'incantesimo significa che non puoi usare la stessa reazione anche per lanciare <i>Controincantesimo</i>. È necessario che un altro incantatore nel gruppo lo contrasti."
+        ]
+    },
+    {
+        title: "Presa**",
         optional: "Homebrew rule",
         icon: "grab",
         subtitle: "Attacco speciale in mischia",
@@ -49,5 +64,34 @@ data_reaction = [
             "Usando almeno una mano libera, cerchi di afferrare il bersaglio effettuando una prova di lotta, una prova di Forza (Atletica) contesa dalla prova di Forza (Atletica) o Destrezza (Acrobazia) del bersaglio (il bersaglio sceglie la capacità da usare).",
             "Se hai successo, sottoponi il bersaglio alla condizione di afferrato (la sua velocità è impostata a 0)."
         ]
-    },    
+    },
+    {
+        title: "Parata**",
+        optional: "Homebrew rule",
+        icon: "grab",
+        subtitle: "Difenditi da un attacco in mischia",
+        description: "Come reazione quando si viene colpiti da un attacco in mischia, si aggiunge un bonus alla CA che potrebbe far sì che l'attacco vada a vuoto.",
+        reference: "Adattato da MM, pgs. 347, 349.",
+        bullets: [
+            "Questa reazione è una caratteristica comune nei blocchi statistiche dei mostri (come la Guardia o il Cavaliere) che i DM possono concedere ai giocatori come talento o regola della casa. Rappresenta la deviazione attiva di un colpo con un'arma.",
+            "Innesco: Un'altra creatura ti colpisce con un attacco in mischia.",
+            "Reazione: Aggiungi un bonus alla CA per quell'attacco, facendo sì che possa potenzialmente mancare il bersaglio. (Il bonus per un PG è spesso impostato sul suo bonus di competenza).",
+            "Devi essere in grado di vedere l'attaccante e impugnare un'arma da mischia.",
+            "A differenza dell'incantesimo Scudo, Parata funziona solo contro un attacco in mischia e il bonus è inferiore, ma non richiede slot incantesimo, il che lo rende uno strumento prezioso per i personaggi marziali."
+        ]
+    },
+    {
+        title: "Mettersi al riparo**",
+        optional: "Homebrew rule",
+        icon: "grab",
+        subtitle: "Evita un effetto ad area",
+        description: "Cadi a terra prono per ottenere vantaggio su un tiro salvezza su Destrezza.",
+        reference: "",
+        bullets: [
+            "Quando una creatura si trova ad affrontare incantesimi con effetto ad area come <i>Palla di Fuoco</i>, può ottenere maggiori probabilità di sopravvivenza finendo a terra.",
+            "Innesco: Sei soggetto a un effetto che richiede un tiro salvezza su Destrezza.",
+            "Reazione: Puoi usare la tua reazione per cadere prono.",
+            "Effetto: Ottieni vantaggio su quel tiro salvezza su Destrezza."
+        ]
+    },
 ]
