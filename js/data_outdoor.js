587d051d0d083d56ffe4065b9ffe3b241c15fdaa
meruleo165 <merula651@gmail.com>
2026-01-13 22:45:18 +0100

    Update data_outdoor.js

--- js/data_outdoor.js
+++ js/data_outdoor.js
@@ -1,16 +1,86 @@
 data_outdoor = [
     {
-        title: "Attacco di opportunità",
+        title: "Riposo",
         optional: "Standard rule",
-        icon: "crossed-swords",
-        subtitle: "Il nemico esce dalla tua portata",
-        description: "Raramente puoi muoverti sconsideratamente oltre i tuoi nemici senza metterti in pericolo.",
-        reference: "PHB, pg. 195.",
+        icon: "night-sleep",
+        subtitle: "Tempo di riposare",
+        description: "Gli avventurieri possono concedersi dei riposi brevi nell'arco della giornata e un riposo lungo alla conclusione della giornata.",
+        reference: "PHB, pg. 186.",
         bullets: [
-            "Innesco: una creatura nemica che puoi vedere esce dalla tua portata.",
-            "Effettua un attacco in mischia contro la creatura che provoca.",
-            "L'attacco interrompe il movimento della creatura che provoca, e si verifica subito prima che la creatura esca dalla tua portata.",
-            "Le creature non provocano un attacco di opportunità quando si teletrasportano o quando qualcuno o qualcosa le sposta senza usare il loro movimento, azione o reazione."
+            "",
+            "<b>Riposo Breve</b>: dura almeno 1 ora, un personaggio può spendere uno o più Dadi Vita alla fine di un riposo breve",
+            "Eroismo Epico: un riposo breve dura 5 minuti, mentre un riposo lungo dura 1 ora. Questa è la scelta ideale per partite ad alta azione e dal ritmo serrato, in cui le risorse si ripristinano rapidamente.",
+            "Realismo Crudo: un riposo breve dura 8 ore, mentre un riposo lungo dura 7 giorni. Questo rende gli incontri più pericolosi e incoraggia una gestione oculata delle risorse."
+        ]
+    },
+    {
+        title: "Varianti al riposo*",
+        optional: "Optional rule",
+        icon: "night-sleep",
+        subtitle: "Cambia il ritmo di gioco",
+        description: "Modifica la durata dei riposi brevi e lunghi, alterando radicalmente il ritmo di una campagna.",
+        reference: "PHB, pg. 186; DMG, pg. 267.",
+        bullets: [
+            "(Regola Opzionale):",
+            "<b>Riposo Breve</b>: dura almeno 1 ora, un personaggio può spendere uno o più Dadi Vita alla fine di un riposo breve",
+            "Eroismo Epico: un riposo breve dura 5 minuti, mentre un riposo lungo dura 1 ora. Questa è la scelta ideale per partite ad alta azione e dal ritmo serrato, in cui le risorse si ripristinano rapidamente.",
+            "Realismo Crudo: un riposo breve dura 8 ore, mentre un riposo lungo dura 7 giorni. Questo rende gli incontri più pericolosi e incoraggia una gestione oculata delle risorse."
+        ]
+    },
+    {
+        title: "Cibo e Acqua",
+        optional: "Optional rule",
+        icon: "night-sleep",
+        subtitle: "Cambia il ritmo di gioco",
+        description: "Modifica la durata dei riposi brevi e lunghi, alterando radicalmente il ritmo di una campagna.",
+        reference: "PHB, pg. 186; DMG, pg. 267.",
+        bullets: [
+            "(Regola Opzionale):",
+            "<b>Riposo Breve</b>: dura almeno 1 ora, un personaggio può spendere uno o più Dadi Vita alla fine di un riposo breve",
+            "Eroismo Epico: un riposo breve dura 5 minuti, mentre un riposo lungo dura 1 ora. Questa è la scelta ideale per partite ad alta azione e dal ritmo serrato, in cui le risorse si ripristinano rapidamente.",
+            "Realismo Crudo: un riposo breve dura 8 ore, mentre un riposo lungo dura 7 giorni. Questo rende gli incontri più pericolosi e incoraggia una gestione oculata delle risorse."
+        ]
+    },
+    {
+        title: "Fabbricare Oggetti",
+        optional: "Optional rule",
+        icon: "night-sleep",
+        subtitle: "Cambia il ritmo di gioco",
+        description: "Modifica la durata dei riposi brevi e lunghi, alterando radicalmente il ritmo di una campagna.",
+        reference: "PHB, pg. 186; XGE, pg. 131.",
+        bullets: [
+            "(Regola Opzionale):",
+            "<b>Riposo Breve</b>: dura almeno 1 ora, un personaggio può spendere uno o più Dadi Vita alla fine di un riposo breve",
+            "Eroismo Epico: un riposo breve dura 5 minuti, mentre un riposo lungo dura 1 ora. Questa è la scelta ideale per partite ad alta azione e dal ritmo serrato, in cui le risorse si ripristinano rapidamente.",
+            "Realismo Crudo: un riposo breve dura 8 ore, mentre un riposo lungo dura 7 giorni. Questo rende gli incontri più pericolosi e incoraggia una gestione oculata delle risorse."
+        ]
+    },
+    {
+        title: "Addestrarsi",
+        optional: "Optional rule",
+        icon: "night-sleep",
+        subtitle: "Cambia il ritmo di gioco",
+        description: "Modifica la durata dei riposi brevi e lunghi, alterando radicalmente il ritmo di una campagna.",
+        reference: "PHB, pg. 187; XGE, 126-127; DMG, pg. 267.",
+        bullets: [
+            "(Regola Opzionale):",
+            "<b>Riposo Breve</b>: dura almeno 1 ora, un personaggio può spendere uno o più Dadi Vita alla fine di un riposo breve",
+            "Eroismo Epico: un riposo breve dura 5 minuti, mentre un riposo lungo dura 1 ora. Questa è la scelta ideale per partite ad alta azione e dal ritmo serrato, in cui le risorse si ripristinano rapidamente.",
+            "Realismo Crudo: un riposo breve dura 8 ore, mentre un riposo lungo dura 7 giorni. Questo rende gli incontri più pericolosi e incoraggia una gestione oculata delle risorse."
+        ]
+    },
+    {
+        title: "Pergamene Magiche",
+        optional: "Optional rule",
+        icon: "night-sleep",
+        subtitle: "Cambia il ritmo di gioco",
+        description: "Modifica la durata dei riposi brevi e lunghi, alterando radicalmente il ritmo di una campagna.",
+        reference: "DMG, pg. 140, 191; XGE, pg. 133; TCE, pg.",
+        bullets: [
+            "(Regola Opzionale):",
+            "<b>Riposo Breve</b>: dura almeno 1 ora, un personaggio può spendere uno o più Dadi Vita alla fine di un riposo breve",
+            "Eroismo Epico: un riposo breve dura 5 minuti, mentre un riposo lungo dura 1 ora. Questa è la scelta ideale per partite ad alta azione e dal ritmo serrato, in cui le risorse si ripristinano rapidamente.",
+            "Realismo Crudo: un riposo breve dura 8 ore, mentre un riposo lungo dura 7 giorni. Questo rende gli incontri più pericolosi e incoraggia una gestione oculata delle risorse."
         ]
     }
   ]
