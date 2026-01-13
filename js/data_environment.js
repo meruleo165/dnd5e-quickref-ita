a6a75b5a18e312b6c5a49de33b17b60afd9f333b
meruleo165 <merula651@gmail.com>
2026-01-13 20:04:12 +0100

    Update data_environment.js

--- js/data_environment.js
+++ js/data_environment.js
@@ -362,4 +362,60 @@ data_environment_outdoor = [
             "Realismo Crudo: un riposo breve dura 8 ore, mentre un riposo lungo dura 7 giorni. Questo rende gli incontri più pericolosi e incoraggia una gestione oculata delle risorse."
         ]
     },
+    {
+        title: "Cibo e Acqua",
+        optional: "Optional rule",
+        icon: "night-sleep",
+        subtitle: "Cambia il ritmo di gioco",
+        description: "Modifica la durata dei riposi brevi e lunghi, alterando radicalmente il ritmo di una campagna.",
+        reference: "PHB, pg. 186; DMG, pg. 267.",
+        bullets: [
+            "(Regola Opzionale):",
+            "<b>Riposo Breve</b>: dura almeno 1 ora, un personaggio può spendere uno o più Dadi Vita alla fine di un riposo breve",
+            "Eroismo Epico: un riposo breve dura 5 minuti, mentre un riposo lungo dura 1 ora. Questa è la scelta ideale per partite ad alta azione e dal ritmo serrato, in cui le risorse si ripristinano rapidamente.",
+            "Realismo Crudo: un riposo breve dura 8 ore, mentre un riposo lungo dura 7 giorni. Questo rende gli incontri più pericolosi e incoraggia una gestione oculata delle risorse."
+        ]
+    },
+    {
+        title: "Fabbricare Oggetti",
+        optional: "Optional rule",
+        icon: "night-sleep",
+        subtitle: "Cambia il ritmo di gioco",
+        description: "Modifica la durata dei riposi brevi e lunghi, alterando radicalmente il ritmo di una campagna.",
+        reference: "PHB, pg. 186; XGE, pg. 131.",
+        bullets: [
+            "(Regola Opzionale):",
+            "<b>Riposo Breve</b>: dura almeno 1 ora, un personaggio può spendere uno o più Dadi Vita alla fine di un riposo breve",
+            "Eroismo Epico: un riposo breve dura 5 minuti, mentre un riposo lungo dura 1 ora. Questa è la scelta ideale per partite ad alta azione e dal ritmo serrato, in cui le risorse si ripristinano rapidamente.",
+            "Realismo Crudo: un riposo breve dura 8 ore, mentre un riposo lungo dura 7 giorni. Questo rende gli incontri più pericolosi e incoraggia una gestione oculata delle risorse."
+        ]
+    },
+    {
+        title: "Addestrarsi",
+        optional: "Optional rule",
+        icon: "night-sleep",
+        subtitle: "Cambia il ritmo di gioco",
+        description: "Modifica la durata dei riposi brevi e lunghi, alterando radicalmente il ritmo di una campagna.",
+        reference: "PHB, pg. 187; XGE, 126-127; DMG, pg. 267.",
+        bullets: [
+            "(Regola Opzionale):",
+            "<b>Riposo Breve</b>: dura almeno 1 ora, un personaggio può spendere uno o più Dadi Vita alla fine di un riposo breve",
+            "Eroismo Epico: un riposo breve dura 5 minuti, mentre un riposo lungo dura 1 ora. Questa è la scelta ideale per partite ad alta azione e dal ritmo serrato, in cui le risorse si ripristinano rapidamente.",
+            "Realismo Crudo: un riposo breve dura 8 ore, mentre un riposo lungo dura 7 giorni. Questo rende gli incontri più pericolosi e incoraggia una gestione oculata delle risorse."
+        ]
+    },
+    {
+        title: "Pergamene Magiche",
+        optional: "Optional rule",
+        icon: "night-sleep",
+        subtitle: "Cambia il ritmo di gioco",
+        description: "Modifica la durata dei riposi brevi e lunghi, alterando radicalmente il ritmo di una campagna.",
+        reference: "DMG, pg. 140, 191; XGE, pg. 133; TCE, pg.",
+        bullets: [
+            "(Regola Opzionale):",
+            "<b>Riposo Breve</b>: dura almeno 1 ora, un personaggio può spendere uno o più Dadi Vita alla fine di un riposo breve",
+            "Eroismo Epico: un riposo breve dura 5 minuti, mentre un riposo lungo dura 1 ora. Questa è la scelta ideale per partite ad alta azione e dal ritmo serrato, in cui le risorse si ripristinano rapidamente.",
+            "Realismo Crudo: un riposo breve dura 8 ore, mentre un riposo lungo dura 7 giorni. Questo rende gli incontri più pericolosi e incoraggia una gestione oculata delle risorse."
+        ]
+    },
 ]
