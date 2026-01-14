b23cb7947955983c583982eb05d64eecbeff6e1e
meruleo165 <merula651@gmail.com>
2026-01-14 16:31:23 +0100

    Update data_outdoor.js

--- js/data_outdoor.js
+++ js/data_outdoor.js
@@ -7,10 +7,9 @@ data_outdoor = [
         description: "Gli avventurieri possono concedersi dei riposi brevi nell'arco della giornata e un riposo lungo alla conclusione della giornata.",
         reference: "PHB, pg. 186.",
         bullets: [
-            "",
-            "<b>Riposo Breve</b>: dura almeno 1 ora, un personaggio può spendere uno o più Dadi Vita alla fine di un riposo breve",
-            "Eroismo Epico: un riposo breve dura 5 minuti, mentre un riposo lungo dura 1 ora. Questa è la scelta ideale per partite ad alta azione e dal ritmo serrato, in cui le risorse si ripristinano rapidamente.",
-            "Realismo Crudo: un riposo breve dura 8 ore, mentre un riposo lungo dura 7 giorni. Questo rende gli incontri più pericolosi e incoraggia una gestione oculata delle risorse."
+            "<b>Riposo Breve</b>: dura almeno 1 ora, un personaggio può spendere uno o più Dadi Vita alla fine di un riposo breve, fino al massimo dei suoi Dadi Vita, che equivale al suo livello del personaggio.",
+            "<b>Riposo Lungo</b>: dura almeno 8 ore (6 ore di sonno e 2 ore di attività poco impegnative), un personaggio alla fine di un Riposo Lungo recupera tutti i punti ferita perduti. Recupera anche i Dadi Vita spesi, fino a un numero pari alla metà del totale del suo personaggio.",
+            "Per ogni Dado Vita speso, il giocatore tira il dado e aggiunge al risultato il modificatore di Costituzione del personaggio, che recupera un numero di punti ferita pari al totale."
         ]
     },
     {
@@ -22,42 +21,38 @@ data_outdoor = [
         reference: "PHB, pg. 186; DMG, pg. 267.",
         bullets: [
             "(Regola Opzionale):",
-            "<b>Riposo Breve</b>: dura almeno 1 ora, un personaggio può spendere uno o più Dadi Vita alla fine di un riposo breve",
-            "Eroismo Epico: un riposo breve dura 5 minuti, mentre un riposo lungo dura 1 ora. Questa è la scelta ideale per partite ad alta azione e dal ritmo serrato, in cui le risorse si ripristinano rapidamente.",
-            "Realismo Crudo: un riposo breve dura 8 ore, mentre un riposo lungo dura 7 giorni. Questo rende gli incontri più pericolosi e incoraggia una gestione oculata delle risorse."
+            "<b>Variante Eroismo epico</b>: un riposo breve dura 5 minuti, mentre un riposo lungo dura 1 ora. Questa è la scelta ideale per partite ad alta azione e dal ritmo serrato, in cui le risorse si ripristinano rapidamente.",
+            "<b>Variante Realismo crudo</b>: un riposo breve dura 8 ore, mentre un riposo lungo dura 7 giorni. Questo rende gli incontri più pericolosi e incoraggia una gestione oculata delle risorse."
         ]
     },
     {
         title: "Cibo e Acqua",
         optional: "Standard rule",
         icon: "meat",
-        subtitle: "Cambia il ritmo di gioco",
-        description: "Modifica la durata dei riposi brevi e lunghi, alterando radicalmente il ritmo di una campagna.",
-        reference: "PHB, pg. 186; DMG, pg. 267.",
+        subtitle: "Rischio di Indebolimento",
+        description: "I personaggi che non mangiano o non bevono subiscono gli effetti dell'indebolimento",
+        reference: "PHB, pg. 185; DMG, pgs. 111-112.",
         bullets: [
-            "<b>Riposo Breve</b>: dura almeno 1 ora, un personaggio può spendere uno o più Dadi Vita alla fine di un riposo breve",
-            "Eroismo Epico: un riposo breve dura 5 minuti, mentre un riposo lungo dura 1 ora. Questa è la scelta ideale per partite ad alta azione e dal ritmo serrato, in cui le risorse si ripristinano rapidamente.",
-            "Realismo Crudo: un riposo breve dura 8 ore, mentre un riposo lungo dura 7 giorni. Questo rende gli incontri più pericolosi e incoraggia una gestione oculata delle risorse."
+            "Un personaggio può fare a meno del cibo per un numero di giorni pari a 3 + il suo modificatore di Costituzione (fino a un minimo di 1). Alla fine di ogni giornata oltre quel limite, un personaggio subisce automaticamente un livello di <i>Indebolimento</i>.",
+            "Un personaggio che beve soltanto la metà dell'ammontare di acqua richiesto deve superare un tiro salvezza su Costituzione con CD 15, altrimenti subisce un livello di <i>Indebolimento</i> alla fine della giornata."
         ]
     },
     {
         title: "Fabbricare Oggetti",
         optional: "Standard rule",
         icon: "power-ring",
-        subtitle: "Cambia il ritmo di gioco",
-        description: "Modifica la durata dei riposi brevi e lunghi, alterando radicalmente il ritmo di una campagna.",
-        reference: "PHB, pg. 186; XGE, pg. 131.",
+        subtitle: "Attività fuori servizio",
+        description: "Fabbrica oggetti non magici, magici, equipaggiamento o opere d'arte.",
+        reference: "PHB, pg. 187; DMG, pgs. 128-129; XGE, pgs. 131-132.",
         bullets: [
-            "<b>Riposo Breve</b>: dura almeno 1 ora, un personaggio può spendere uno o più Dadi Vita alla fine di un riposo breve",
-            "Eroismo Epico: un riposo breve dura 5 minuti, mentre un riposo lungo dura 1 ora. Questa è la scelta ideale per partite ad alta azione e dal ritmo serrato, in cui le risorse si ripristinano rapidamente.",
-            "Realismo Crudo: un riposo breve dura 8 ore, mentre un riposo lungo dura 7 giorni. Questo rende gli incontri più pericolosi e incoraggia una gestione oculata delle risorse."
+            "Work in Progress. è troppa roba."
         ]
     },
     {
         title: "Addestrarsi",
         optional: "Standard rule",
         icon: "target-dummy",
-        subtitle: "Cambia il ritmo di gioco",
+        subtitle: "Attività fuori servizio",
         description: "Modifica la durata dei riposi brevi e lunghi, alterando radicalmente il ritmo di una campagna.",
         reference: "PHB, pg. 187; XGE, 126-127; DMG, pg. 267.",
         bullets: [
@@ -70,14 +65,17 @@ data_outdoor = [
         title: "Pergamene Magiche*",
         optional: "Optional rule",
         icon: "scroll-quill",
-        subtitle: "Cambia il ritmo di gioco",
-        description: "Modifica la durata dei riposi brevi e lunghi, alterando radicalmente il ritmo di una campagna.",
-        reference: "DMG, pg. 140, 191; XGE, pg. 133; TCE, pg.",
+        subtitle: "Crea o copia una pergamena",
+        description: "Le pergamene magiche possono essere copiate o create.",
+        reference: "PHB, pg. 83; DMG, pg. 140, 191; XGE, pg. 133; TCE, pgs. 53-55.",
         bullets: [
             "(Regola Opzionale):",
-            "<b>Riposo Breve</b>: dura almeno 1 ora, un personaggio può spendere uno o più Dadi Vita alla fine di un riposo breve",
-            "Eroismo Epico: un riposo breve dura 5 minuti, mentre un riposo lungo dura 1 ora. Questa è la scelta ideale per partite ad alta azione e dal ritmo serrato, in cui le risorse si ripristinano rapidamente.",
-            "Realismo Crudo: un riposo breve dura 8 ore, mentre un riposo lungo dura 7 giorni. Questo rende gli incontri più pericolosi e incoraggia una gestione oculata delle risorse."
+            "Un incantesimo da mago su una pergamena può essere copiato proprio come gli incantesimi nei libri degli incantesimi. Quando un incantesimo viene copiato da una pergamena, chi lo copia deve superare una prova di Intelligenza (Arcano) con una CD pari a 10 + il livello dell'incantesimo.",
+            "Se la prova riesce, l'incantesimo è copiato con successo. Che la prova abbia successo o meno, la pergamena viene distrutta.",
+            "Per trascrivere una pergamena magica, è richiesto un ammontare di tempo e denaro dipendente dall'incantesimo che il personaggio vuole trascrivere, come indicato nella tabella Costi delle Pergamene Magiche.",
+            "Il personaggio deve avere competenza nell'abilità Arcano e deve fornire le eventuali componenti materiali richieste per lanciare l'incantesimo.",
+            "Il personaggio deve avere preparato l'incantesimo o averlo tra i propri incantesimi conosciuti, al fine di scrivere una pergamena che lo contenga.",
+            "Se l'incantesimo trascritto è un trucchetto, la versione sulla pergamena funziona come se l'incantatore fosse di 1° livello."
         ]
     }
   ]
